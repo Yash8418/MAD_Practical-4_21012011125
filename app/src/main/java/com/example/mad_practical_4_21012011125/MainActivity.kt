6dfd6c0dc32a196dee79ab266e5215eabad5dddc
student <student@gnu.ac.in>
2023-08-07 10:46:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_4_21012011125/MainActivity.kt
@@ -0,0 +1,13 @@
+ package com.example.mad_practical_4_21012011125
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+ class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
