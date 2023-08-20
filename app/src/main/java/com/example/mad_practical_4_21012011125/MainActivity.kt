5644d7e128bf27d3c2dbf3115b286b8c7409580a
Yash <y.d.prajapati04@gmail.com>
2023-08-20 21:09:16 +0530

    Updated

--- app/src/main/java/com/example/mad_practical_4_21012011125/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_21012011125/MainActivity.kt
@@ -8,18 +8,18 @@ import android.widget.Button
 
  class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-//        val login:Button=findViewById(R.id.login)
-//        login.setOnClickListener{
-//            intent(this@MainActivity,LoginActivity::class.java).also{startActivity(intent)}
-//        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val login:Button=findViewById(R.id.login)
+        login.setOnClickListener {
+            intent = Intent(this, LoginActivity::class.java).also { startActivity(intent) }
+        }
+        val signup:Button=findViewById(R.id.signup)
+        signup.setOnClickListener {
+            intent = Intent(this, RegistrationActivity::class.java).also { startActivity(intent) }
+        }
 
     }
 
-     fun loginClick(view: View){
-         val intent= Intent(this,LoginActivity::class.java)
-         startActivity(intent)
 
-     }
 }
